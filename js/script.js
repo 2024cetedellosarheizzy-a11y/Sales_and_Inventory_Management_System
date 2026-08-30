06f6d1b4c1ddb48eb6ae360f576f6cfb4a492550
RHEIZZY DELLOSA <2024cetedellosarheizzy@online.htcgsc.edu.ph>
2026-08-31 05:00:05 +0800

    Create initial sales and inventory interface

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const getStartedButton =
+        document.getElementById("getStartedBtn");
+
+    getStartedButton.addEventListener("click", function () {
+
+        alert(
+            "Welcome to the Sales and Inventory Management System!"
+        );
+
+    });
+
+});
